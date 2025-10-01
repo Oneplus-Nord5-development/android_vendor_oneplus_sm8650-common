0ee5191bbdb5343acdfd1d12b197256259e9bb26
grep <grepfox@tutamail.com>
2025-10-01 07:22:20 +0000

    sm8650-common: Sync with device tree
    Change-Id: I45a0cd9305c4dbf3703bdd0fabcd25e4a16852da

--- proprietary/odm/bin/init.oplus.wifi.sh
+++ proprietary/odm/bin/init.oplus.wifi.sh
@@ -77,16 +77,12 @@ else
 	persist_version=0
 fi
 
-first_boot=`getprop persist.vendor.oplus.first.boot`
-echo "xx${first_boot}" > /dev/kmsg
-if [ "${first_boot}x" = "x" ] || [ ! -s "$targetFile" -o $system_version -gt $persist_version ]; then
+
+if [ ! -s "$targetFile" -o $system_version -gt $persist_version ]; then
     cp $sourceFile  $targetFile
     sync
     chown system:wifi $targetFile
     chmod 666 $targetFile
-    echo "oplus_wlan ini version: ${system_version}" > /dev/kmsg
-else
-    echo "oplus_wlan ini version: ${persist_version}" > /dev/kmsg
 fi
 
 persistini=`cat "$targetFile" | grep -v "#" | grep -wc "END"`
